8010e95090713471f9b5452e31bea9a4a5e3d509
Camila Cristina <criscamila09@gmail.com>
2022-06-29 18:27:21 -0300

    conclusão de uma tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,16 @@ const App = () => {
     }
   ])
 
+  const handleTaskClick = taskId => {
+    const newTasks = tasks.map(task => {
+      if (task.id === taskId) return { ...task, completed: !task.completed }
+
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
   const handleTaskAddition = taskTitle => {
     const newTasks = [
       ...tasks,
@@ -45,7 +55,7 @@ const App = () => {
     <>
       <div className="container">
         <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
       </div>
     </>
   )
