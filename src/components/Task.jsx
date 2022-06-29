b306317c2189bb8379389dad8ac5ac98976806a4
Camila Cristina <criscamila09@gmail.com>
2022-06-29 18:48:35 -0300

    add react icons

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CgClose } from 'react-icons/cg'
 import './Task.css'
 
 const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
@@ -16,7 +17,7 @@ const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
           className="remove-task-button"
           onClick={() => handleTaskDeletion(task.id)}
         >
-          X
+          <CgClose />
         </button>
       </div>
     </div>
