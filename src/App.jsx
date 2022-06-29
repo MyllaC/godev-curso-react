c37ef12e00f292b0a2948d9b595c45157f028f70
Camila Cristina <criscamila09@gmail.com>
2022-06-29 18:08:16 -0300

    uuidv4 e limpar o input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import './App.css'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -32,7 +33,7 @@ const App = () => {
       ...tasks,
       {
         title: taskTitle,
-        id: Math.random(10),
+        id: uuidv4(),
         completed: false
       }
     ]
